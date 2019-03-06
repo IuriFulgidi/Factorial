4fc806f4ef2acc2203769dca0fc02f5e1f9b7b21
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-03-06 09:13:27 +0100

    Metodo testato per numeri fattoriali

---
+++ UnitTestProject1/CalcoliCombinatori.Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquazioniLibrary;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataTestMethod]
+        [DataRow(5, 120)]
+        [DataRow(1, 1)]
+        [DataRow(0, 1)]
+        [DataRow(10, 3628800)]
+        [DataRow(20, 2432902008176640000)]
+
+        [TestMethod]
+        public void TestFatt_1(long num, long exp)
+        {
+            long obt = CalcoliCombinatori.Fatt(num);
+            Assert.AreEqual(exp, obt);
+        }
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("32f7accb-9d4f-4b0e-8eb1-df5b8ca1cfcf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
